0c92d672f9912192ca4eb434342b52780c3cd317
Jeongho Han <jungho1109@gmail.com>
2019-11-30 21:25:48 -0800

    Initial route

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import express from "express";
-import {connectDB} from './config/db';
+import { router as userRouter } from "./api/users";
+import { router as profileRouter } from "./api/profile";
+import { router as postRouter } from "./api/posts";
+import { router as authRouter } from "./api/auth";
+import { connectDB } from "./config/db";
 
 const app = express();
 
@@ -8,6 +12,12 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API Running!"));
 
-const PORT = process.env.PORT||5000;
+// Define Routes
+app.use("/api/users", userRouter);
+app.use("/api/profile", profileRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/auth", authRouter);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
