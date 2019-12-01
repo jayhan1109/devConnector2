7cc41c2dae500e3b0812cfb9a509393f08bcd483
Jeongho Han <jungho1109@gmail.com>
2019-11-30 22:10:18 -0800

    Register with jsonwebtoken

--- api/users.js
+++ api/users.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { check, validationResult } from "express-validator";
-import gravatar from 'gravatar';
-import bcrypt from 'bcryptjs';
+import gravatar from "gravatar";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "config";
 import { User } from "../models/User";
 
 export const router = express.Router();
@@ -34,29 +36,48 @@ router.post(
       // **** use "let" instead of "const" ***
       let user = await User.findOne({ email });
       if (user) {
-        return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
 
       // Get users gravatar
-      const avatar = gravatar.url(email,{
-          s:'200',
-          r:'pg',
-          d:'mm'
-      })
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
 
       user = new User({
-          name,email,avatar,password
-      })
+        name,
+        email,
+        avatar,
+        password
+      });
 
       // Encrypt password
       const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password,salt);
+      user.password = await bcrypt.hash(password, salt);
 
       // Save user in database
-      await user.save()
+      await user.save();
 
       // Return jsonwebtoken
-      res.send('User Registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtToken"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
