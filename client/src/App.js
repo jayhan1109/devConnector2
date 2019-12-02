0abfeab27bb1b10d635481b245cb874108a4a7cb
Jeongho Han <jungho1109@gmail.com>
2019-12-02 09:39:44 -0800

    Load User & set Auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment,useEffect } from "react";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import { Route, Switch } from "react-router-dom";
@@ -6,8 +6,22 @@ import "./App.css";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import setAuthToken from "./utils/setAuthToken";
+import{loadUser} from './reducers/auth';
+import {store} from './index'
+
+
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
 
 const App = () => {
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, [])
+
   return (
     <Fragment>
       <Navbar />
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { setAlert } from "./alert";
+import setAuthToken from "../utils/setAuthToken";
 
 export const REGISTER_SUCCESS = "auth/REGISTER_SUCCESS";
 export const REGISTER_FAIL = "auth/REGISTER_FAIL";
+export const USER_LOADED = "auth/USER_LOADED";
+export const AUTH_ERROR = "auth/AUTH_ERROR";
+
 
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
@@ -33,6 +37,25 @@ export const register = ({ name, email, password }) => async dispatch => {
   }
 };
 
+// Load User
+export const loadUser=()=>async dispatch=>{
+    if(localStorage.token){
+        setAuthToken(localStorage.token);
+    }
+
+    try {
+        const res = await axios.get('/api/auth');
+        dispatch({
+            type:USER_LOADED,
+            payload:res.data
+        })
+    } catch (err) {
+        dispatch({
+            type:AUTH_ERROR
+        })
+    }
+}
+
 const initialState = {
   token: localStorage.getItem("token"),
   isAuthenticated: null,
@@ -44,6 +67,13 @@ export default function auth(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case USER_LOADED:
+        return{
+            ...state,
+            isAuthenticated:true,
+            loading:false,
+            user:payload
+        }
     case REGISTER_SUCCESS:
       localStorage.setItem("token", payload.token);
       return {
@@ -53,6 +83,7 @@ export default function auth(state = initialState, action) {
         loading: false
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem("token");
       return {
         ...state,
