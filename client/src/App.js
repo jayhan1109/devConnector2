109daf3bdea11dcc0be9e03deaaf436595b7e41a
Jeongho Han <jungho1109@gmail.com>
2019-12-01 22:47:20 -0800

    Set react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import React,{Fragment} from 'react';
-import Navbar from './components/Navbar';
-import Landing from './components/Landing';
-import './App.css';
+import React, { Fragment } from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const App = () => {
   return (
     <Fragment>
-      <Navbar/>
-      <Landing/>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
     </Fragment>
   );
 };
