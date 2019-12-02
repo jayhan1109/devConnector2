d3c8389f534dca1013d2e059fc16af998430b013
Jeongho Han <jungho1109@gmail.com>
2019-12-02 15:51:45 -0800

    profile page

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,53 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
+import { getProfileById } from "../../reducers/profile";
+
+const Profile = ({
+  getProfileById,
+  match,
+  auth,
+  profile: { profile, loading }
+}) => {
+  useEffect(() => {
+    getProfileById(match.params.id);
+  }, [getProfileById, match.params.id]);
+  return (
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to="/profiles" className="btn btn-light">
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                  Edit Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+export default connect(
+  state => ({
+    profile: state.profile,
+    auth: state.auth
+  }),
+  {
+    getProfileById
+  }
+)(Profile);
