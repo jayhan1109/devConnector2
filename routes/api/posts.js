bf369ddacd31c3722464aff069306d1c2c3a481d
Jeongho Han <jungho1109@gmail.com>
2019-12-01 20:33:45 -0800

    Create a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,48 @@
-import express from 'express';
-
+import express from "express";
+import { check, validationResult } from "express-validator";
+import { auth } from "../../middleware/auth";
+import {User} from '../../models/User'
+import {Post} from '../../models/Post'
+import {Profile} from '../../models/Profile'
 
 export const router = express.Router();
 
-// @route   GET     api/posts
-// @desc    Test    route
-// @access  Public
-router.get('/',(req,res)=>{
-    
-})
+// @route   POST     api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+      const errors= validationResult(req);
+      if(!errors.isEmpty()){
+          return res.status(400).json({errors:errors.array()});
+      }
+
+      try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost=new Post({
+            text:req.body.text,
+            name:user.name,
+            avatar:user.avatar,
+            user:req.user.id
+        })
+
+        const post = await newPost.save();
+        res.send(post);
+
+      } catch (err) {
+          console.error(err.message);
+          res.status(500).send('Server Error');
+      }
+  }
+);
+
