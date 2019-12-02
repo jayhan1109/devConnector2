c104872e8b0271a9cdfcc964aae4d4c87e639b60
Jeongho Han <jungho1109@gmail.com>
2019-12-01 22:39:47 -0800

    Initial Client setting

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React,{Fragment} from 'react';
+import Navbar from './components/Navbar';
+import Landing from './components/Landing';
+import './App.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Navbar/>
+      <Landing/>
+    </Fragment>
+  );
+};
+
+export default App;
