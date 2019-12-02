3cc810c517f9784e4c886030477c80080597f980
Jeongho Han <jungho1109@gmail.com>
2019-12-02 11:46:44 -0800

    create-profile component

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import{loadUser} from './reducers/auth';
 import {store} from './index'
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 
 
 
@@ -35,6 +36,7 @@ const App = () => {
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
           <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <PrivateRoute exact path="/create-profile" component={CreateProfile} />
         </Switch>
       </section>
     </Fragment>
