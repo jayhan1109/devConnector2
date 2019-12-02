d12b47e9051865bc8d01c87c37772d539ef10317
Jeongho Han <jungho1109@gmail.com>
2019-12-01 16:43:15 -0800

    Create and Update profile

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ export const connectDB =async()=>{
     try {
         await mongoose.connect(db,{
             useUnifiedTopology:true,
-            useCreateIndex:true
+            useCreateIndex:true,
+            useFindAndModify:false
         });
         console.log('MongoDB Connected...');
         
