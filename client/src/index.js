c13b77db7c5416c988979f6d1f4bad43c3cd7f4e
Jeongho Han <jungho1109@gmail.com>
2019-12-01 23:20:09 -0800

    Redux store

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from "redux-thunk";
+import rootReducer from './reducers/index';
+
+const logger = createLogger();
+
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(logger, ReduxThunk))
+  );
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
