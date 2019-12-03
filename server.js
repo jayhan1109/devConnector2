9b87ac3ca1c3bc0f37ae6aad56daf08ae33d0f39
Jeongho Han <jungho1109@gmail.com>
2019-12-03 14:21:29 -0800

    Ready for deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { router as profileRouter } from "./routes/api/profile";
 import { router as postRouter } from "./routes/api/posts";
 import { router as authRouter } from "./routes/api/auth";
 import { connectDB } from "./config/db";
+import path from 'path';
 
 const app = express();
 
@@ -12,13 +13,21 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running!"));
-
 // Define Routes
 app.use("/api/users", userRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/auth", authRouter);
+
+// Serve static assets in production
+if(process.env.NODE_ENV==='production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
