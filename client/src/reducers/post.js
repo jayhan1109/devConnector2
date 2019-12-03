c855747a7eec0497aac7ab0b87177b3652124cb3
Jeongho Han <jungho1109@gmail.com>
2019-12-03 13:24:12 -0800

    Add Comment

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,10 +2,13 @@ import axios from "axios";
 import { setAlert } from "./alert";
 
 export const GET_POSTS = "post/GET_POSTS";
+export const GET_POST = "post/GET_POST";
 export const POST_ERROR = "post/POST_ERROR";
 export const UPDATE_LIKES = "post/UPDATE_LIKES";
 export const DELETE_POST = "post/DELETE_POST";
 export const ADD_POST = "post/ADD_POST";
+export const ADD_COMMENT = "post/ADD_COMMENT";
+export const DELETE_COMMENT = "post/DELETE_COMMENT";
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -31,7 +34,7 @@ export const addLike = id => async dispatch => {
 
     dispatch({
       type: UPDATE_LIKES,
-      payload: {id,likes:res.data}
+      payload: { id, likes: res.data }
     });
   } catch (err) {
     dispatch({
@@ -48,7 +51,7 @@ export const removeLike = id => async dispatch => {
 
     dispatch({
       type: UPDATE_LIKES,
-      payload: {id,likes:res.data}
+      payload: { id, likes: res.data }
     });
   } catch (err) {
     dispatch({
@@ -67,7 +70,7 @@ export const deletePost = id => async dispatch => {
       type: DELETE_POST,
       payload: id
     });
-    dispatch(setAlert('Post Removed','success'));
+    dispatch(setAlert("Post Removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
@@ -77,20 +80,82 @@ export const deletePost = id => async dispatch => {
 };
 
 // Add post
-export const addPost = formData=> async dispatch => {
-  const config={
-    headers:{
-      'Content-Type':'application/json'
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
     }
-  }
+  };
   try {
-    const res = await axios.post(`/api/posts`,formData,config);
+    const res = await axios.post(`/api/posts`, formData, config);
 
     dispatch({
       type: ADD_POST,
       payload: res.data
     });
-    dispatch(setAlert('Post Created','success'));
+    dispatch(setAlert("Post Created", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get a post
+export const getPost = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Add comment
+export const addComment = (postId, formData) => async dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  try {
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    );
+
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data
+    });
+    dispatch(setAlert("Comment Created", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+    dispatch({
+      type: DELETE_COMMENT,
+      payload: commentId
+    });
+    dispatch(setAlert("Comment Removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
@@ -108,7 +173,7 @@ const initialState = {
 
 export default function post(state = initialState, action) {
   const { type, payload } = action;
-  
+
   switch (type) {
     case GET_POSTS:
       return {
@@ -116,30 +181,55 @@ export default function post(state = initialState, action) {
         posts: payload,
         loading: false
       };
+    case GET_POST:
+      return {
+        ...state,
+        post: payload,
+        loading: false
+      };
     case ADD_POST:
-      return{
+      return {
         ...state,
-        posts:[payload,...state.posts],
-        loading:false 
-      }
+        posts: [payload, ...state.posts],
+        loading: false
+      };
     case DELETE_POST:
-      return{
+      return {
         ...state,
-        posts:state.posts.filter(post=>post._id!==payload),
-        loading:false
-      }
+        posts: state.posts.filter(post => post._id !== payload),
+        loading: false
+      };
     case POST_ERROR:
       return {
         ...state,
         error: payload,
         loading: false
       };
-      case UPDATE_LIKES:
-        return{
-          ...state,
-          posts:state.posts.map(post=>post._id===payload.id?{...post,likes:payload.likes}:post),
-          loading:false
-        }
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map(post =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
+        loading: false
+      };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false
+      };
+    case DELETE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            comment => comment._id !== payload
+          )
+        },
+        loading: false
+      };
     default:
       return state;
   }
