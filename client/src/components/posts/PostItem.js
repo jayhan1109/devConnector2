cc4f0aff1929345c9f911c11129e7f1208b3a863
Jeongho Han <jungho1109@gmail.com>
2019-12-02 22:34:34 -0800

    Like & Unlike post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,10 +3,13 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { connect } from "react-redux";
+import {addLike,removeLike} from '../../reducers/post';
 
 const PostItem = ({
   auth,
-  post: { _id, text, name, avatar, user, likes, comments, date }
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  addLike,
+  removeLike
 }) => {
   return (
     <div className="post bg-white p-1 my-1">
@@ -21,13 +24,13 @@ const PostItem = ({
         <p className="post-date">
           Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
-        <button type="button" className="btn btn-light">
+        <button onClick={e=>addLike(_id)} type="button" className="btn btn-light">
           <i className="fas fa-thumbs-up"></i> {' '}
           {likes.length > 0 && (
             <span>{likes.length}</span>
           )}
         </button>
-        <button type="button" className="btn btn-light">
+        <button onClick={e=>removeLike(_id)} type="button" className="btn btn-light">
           <i className="fas fa-thumbs-down"></i>
         </button>
         <Link to={`/post/${_id}`} className="btn btn-primary">
@@ -48,12 +51,17 @@ const PostItem = ({
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  addLike:PropTypes.func.isRequired,
+  removeLike:PropTypes.func.isRequired,
 };
 
 export default connect(
   state => ({
     auth: state.auth
   }),
-  null
+  {
+      addLike,
+      removeLike
+  }
 )(PostItem);
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -3,6 +3,7 @@ import { setAlert } from "./alert";
 
 export const GET_POSTS = "post/GET_POSTS";
 export const POST_ERROR = "post/POST_ERROR";
+export const UPDATE_LIKES = "post/UPDATE_LIKES";
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -12,6 +13,40 @@ export const getPosts = () => async dispatch => {
     dispatch({
       type: GET_POSTS,
       payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Add like
+export const addLike = id => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: {id,likes:res.data}
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Remove like
+export const removeLike = id => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: {id,likes:res.data}
     });
   } catch (err) {
     dispatch({
@@ -44,6 +79,12 @@ export default function post(state = initialState, action) {
         error: payload,
         loading: false
       };
+      case UPDATE_LIKES:
+        return{
+          ...state,
+          posts:state.posts.map(post=>post._id===payload.id?{...post,likes:payload.likes}:post),
+          loading:false
+        }
 
     default:
       return state;
